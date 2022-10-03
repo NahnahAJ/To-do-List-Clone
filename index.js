dcd396b6866a4c96a65729ce5d4d1db2685684f3
Nahnah Ahjuwah <nahnahahjuwah@gmail.com>
2022-10-03 12:50:57 -0700

    Create a clone of my to-do-list repo

---
+++ index.js
@@ -0,0 +1,44 @@
+import populate from './modules/populateFunction.js';
+import tasks from './modules/objects.js';
+import loadTasks from './modules/displayfromLS.js';
+
+const newList = document.querySelector('[data-new-list]');
+const newInput = document.querySelector('[data-new-input]');
+const clear = document.querySelector('.clear');
+
+const saveToStorage = (newTask) => {
+  localStorage.setItem('store_now', JSON.stringify(newTask));
+};
+
+let totaltasks = tasks.length;
+const indexFunc = () => {
+  totaltasks += 1;
+  return totaltasks;
+};
+
+const createNewList = (name) => ({ index: indexFunc(), description: name, completed: false });
+
+newList.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const listValue = newInput.value;
+  if (listValue === null || listValue === '') return;
+  // holder.innerHTML = '';
+  const list = createNewList(listValue);
+  populate(list);
+  tasks.push(list);
+  saveToStorage(tasks);
+  newList.reset();
+});
+
+// On load Display all Tasks
+loadTasks();
+
+// Function to clear all checked boxes
+clear.addEventListener('click', () => {
+  const tasksAll = JSON.parse(localStorage.getItem('store_now'));
+  // const completedItem = document.querySelectorAll('.completed');
+  // completedItem.forEach((val) => val.parentElement.remove());
+  const filteredList = tasksAll.filter((obj) => obj.completed !== true);
+  localStorage.setItem('store_now', JSON.stringify(filteredList));
+  window.location.reload();
+});
